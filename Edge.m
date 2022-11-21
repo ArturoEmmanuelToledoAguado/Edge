d5fe4af8b1c4ed7e5e8024546bab20a1ff75d90f
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-20 18:41:07 -0600

    Fin del primer prototipo

--- Edge.m
+++ Edge.m
@@ -2,3 +2,14 @@ clc;
 clear all;
 close all;
 
+%Extraccion de las imagenes
+ruta=["IMG/1.jpg", "IMG/2.jpg", "IMG/3.jpg", "IMG/4.jpg", "IMG/5.jpg", "IMG/6.jpg", "IMG/7.jpg"];
+
+%Abrir la imagen
+img=imread(ruta(1));
+imshow(img)
+
+Bw=rgb2gray(img);
+Bw=edge(Bw);
+figure
+imshow(Bw)
