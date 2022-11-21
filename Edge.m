f6ad76b47c1c833cd5ff6c951269901674333cae
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-20 19:02:16 -0600

    Fin del prototipo

--- Edge.m
+++ Edge.m
@@ -5,11 +5,36 @@ close all;
 %Extraccion de las imagenes
 ruta=["IMG/1.jpg", "IMG/2.jpg", "IMG/3.jpg", "IMG/4.jpg", "IMG/5.jpg", "IMG/6.jpg", "IMG/7.jpg"];
 
-%Abrir la imagen
-img=imread(ruta(1));
-imshow(img)
+%Operadores
+op=["Sobel", "Prewitt", "Roberts", "log", "zerocross", "Canny", "approxcanny"];
 
-Bw=rgb2gray(img);
-Bw=edge(Bw);
-figure
-imshow(Bw)
+for i=1:7;
+    %Abrir la imagen
+    img=imread(ruta(i));
+    %Pasamos a gris
+    img=rgb2gray(img);
+    figure(i)
+    subplot(2,1,1)
+    imshow(img)
+    
+    %for para los metodos
+    for j=1:7
+        Bw=edge(img,op(j));
+        if j<2
+            subplot(2,1,2)
+            imshow(Bw)
+        elseif j>=2 && j<4
+            figure(i+7)
+            subplot(2,1,j-1)
+            imshow(Bw)
+        elseif j>=4 && j<6
+            figure(i+14)
+            subplot(2,1,j-3)
+            imshow(Bw)
+        else
+            figure(i+21)
+            subplot(2,1,j-5)
+            imshow(Bw)
+        end
+    end
+end
